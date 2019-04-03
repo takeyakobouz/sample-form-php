517c1884aee9be363c4e0d4c3b7709aa546cfaca
takuya suetake <takuya.suetake.acter@gmail.com>
2019-04-03 11:40:02 +0900

    Html不要な部分を消す
    Php使うところまずは一つに

--- index.php
+++ index.php
@@ -10,61 +10,35 @@
     <header>
       <h1>blog</h1>
     </header>
-    <section>
-      <p class="wellcome_text">ようこそ
-        <a href="#"  class="succes!">
-          <?php
-            if ($_GET["name"]){
-              $name = $_GET["name"];
-              echo $name;
-            }else{
-              echo "no read";
-            }
 
-          ?>
-       </a>さん！<p>
-    <form action ="index.php" method="get">
-     <input type="text" name ="name" placeholder="名前を入力" ><br/>
-     <input type="submit" value="send">
-    </p>
-    </section>
-    <footer>
-     <p>
-       <ul>
-         <li><a href="#"  class="succes!">
-           <?php
-            if ($_GET["name"]){
-              $name = $_GET["name"];
-              echo $name;
-            }else{
-              echo "no read";
-            }
+      <section>
+        <p class="wellcome_text">welcome<a href="#"  class="succes!"></a></p>
+        <form action ="index.php" method="get">
+          <input type="text" name ="name" placeholder="名前を入力" ><br/>
+          <input type="submit" value="send">
 
-          ?>さんのマイページを見る
-        </a></li>
-         <li><a href="#">NEWS</a></li>
-         <li><a href="#">BLOG</a></li>
-       </ul>
+          <!-- 入力を表示 -->
+        </form>
+      </section>
 
-       <a href="#">ヘルプ</a>
-       <a href="#">利用規約</a>
-       <a href="#">お問い合わせ</a>
-     </p>
-     <p>copyright <a href="#"  class="succes!">
-       <?php
-            if ($_GET["name"]){
-              $name = $_GET["name"];
-              echo $name;
-            }else{
-              echo "no read";
-            }
+      <footer>
+         <div class="contets">
+           <ul>
+             <li><a href="#">NEWS</a></li>
+             <li><a href="#">BLOG</a></li>
+           </ul>
+         </div>
 
-          ?>
-    </a></p>
-   </footer>
-    <?php
-      echo "hello";
-      ?>
-</wrapper>
+         <div class="about">
+           <ul>
+             <li><a href="#">ヘルプ</a></li>
+             <li><a href="#">利用規約</a></li>
+             <li><a href="#">お問い合わせ</a></li>
+           </ul>
+         </div>
+
+        <p>copyright <a href="#"  class="succes!">takeyakobouz</a></p>
+      </footer>
+    </wrapper>
   </body>
 </html>
